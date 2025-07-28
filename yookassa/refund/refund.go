19c3512a199acebb61e670987a93c718473885c2
Blaise Popov <blaise.popov@gmail.com>
2025-07-28 15:53:55 +0300

    chore: переименования пакета и обновление путей импортов

--- yookassa/refund/refund.go
+++ yookassa/refund/refund.go
@@ -2,7 +2,7 @@
 package yoorefund
 
 import (
-	"github.com/rvinnie/yookassa-sdk-go/yookassa/common"
+	"github.com/BlaisePopov/yookassa-sdk-go/yookassa/common"
 	"time"
 )
 
--- yookassa/refunds.go
+++ yookassa/refunds.go
@@ -7,8 +7,8 @@ import (
 	"io"
 	"net/http"
 
-	yooerror "github.com/rvinnie/yookassa-sdk-go/yookassa/errors"
-	yoorefund "github.com/rvinnie/yookassa-sdk-go/yookassa/refund"
+	yooerror "github.com/BlaisePopov/yookassa-sdk-go/yookassa/errors"
+	yoorefund "github.com/BlaisePopov/yookassa-sdk-go/yookassa/refund"
 )
 
 const (
